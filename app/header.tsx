571859b2f8f6e75a84113d3c60a740834b4bbbe5
Yan Fernandes <102800044+yan-pi@users.noreply.github.com>
2025-11-14 12:36:58 -0300

    chore: new blogpost

--- app/header.tsx
+++ app/header.tsx
@@ -22,7 +22,7 @@ export function Header() {
           className="tect-lg text-[var(--color-muted-foreground)]"
           delay={0.5}
         >
-          Staff Engineer
+          Tech Guy
         </TextEffect>
       </div>
     </header>
